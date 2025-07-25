a182d9431bd3fb372276f3560443a87c0ec7bd8f
Ange-205 <angefrankabayi@gmail.com>
2025-07-25 16:49:29 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const add = require('./index');
 
-if (add(2, 3) !== 6) {
-  throw new Error("Test échoué : 2 + 3 devrait donner 6");
+if (add(2, 3) !== 5) {
+  throw new Error("Test échoué : 2 + 3 devrait donner 5");
 }
 
 console.log("✅ Test réussi !");
